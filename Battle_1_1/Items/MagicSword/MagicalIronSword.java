e50170914ed53838bfd9c76412bf7a201e1a3431
170135 <34013932+170135@users.noreply.github.com>
2017-11-28 16:03:40 +0900

    Battle

---
+++ Battle_1_1/Items/MagicSword/MagicalIronSword.java
@@ -0,0 +1,7 @@
+package Items.MagicSword;
+import Items.ItemsOrigin;
+public class MagicalIronSword extends ItemsOrigin{
+	public MagicalIronSword(){
+		super("魔鉄の剣", new int[] {0,1,} new int[] {10,10,10,10,10,10,10,10}, 10, "");
+	}
+}
