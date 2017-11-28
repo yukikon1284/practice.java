e2bb6fa79b71eb472aa7771749e18900fe587995
yukikon1284 <33075631+yukikon1284@users.noreply.github.com>
2017-11-28 14:08:24 +0900

    Orizin

--- Battle_1_1/Items/ItemsOrigin.java
+++ Battle_1_1/Items/ItemsOrigin.java
@@ -3,11 +3,15 @@ public class ItemsOrigin{
 	private String name;
 	private int[] type;
 	private int[] addAbility;
-	
-	ItemsOrigin(final String name, final int[] type, final int[] addAbility){
+	private int money;
+	private String description;
+	//ItemsOrigin(名前, ,,);
+	ItemsOrigin(final String name, final int[] type, final int[] addAbility, int money, String description){
 		this.name = name;
 		this.type = type;
 		this.addAbility = addAbility;
+		this.money = money;
+		this.description = description;
 	}
 	String getName(){
 		return this.name;
