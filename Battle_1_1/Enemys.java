c91709ba6b87e2360f9f5348999e8c5bd9b8ee38
yukikon1284 <33075631+yukikon1284@users.noreply.github.com>
2017-11-30 17:06:52 +0900

    Battle

--- Battle_1_1/Enemys.java
+++ Battle_1_1/Enemys.java
@@ -5,12 +5,27 @@ public class Enemys{
 	private static String[] enemySize = {"小","中","大"};
 	Enemys(){
 		EnemysOrigin = new EnemysOrigin[] {new Slime()};
-		System.out.println(EnemysOrigin[0].getName());
+		String[] name = Case.getDesignationName(getAllName(), getAllType(), 0);
+		for (int i=0; i<name.length; i++) {
+			Case.pl(name[i]);
+		}
 	}
 	static void seyDesignationType(final int place){
 		for (int i=0; i<EnemysOrigin.length; i++) {
 			final int type = EnemysOrigin[i].getType();
 			if (type == place) System.out.println(enemySize[type]);
 		}
+	}
+	static int[] getAllType(){
+		final int IOL = EnemysOrigin.length;
+		final int[] type = new int[IOL];
+		for (int i=0; i<IOL; i++) type[i] = EnemysOrigin[i].getType();
+		return type;
+	}
+	static String[] getAllName(){
+		final int IOL = EnemysOrigin.length;
+		final String[] name = new String[IOL];
+		for (int i=0; i<IOL; i++) name[i] = EnemysOrigin[i].getName();
+		return name;
 	}
 }
--- Battle_1_1/Items/ItemsOrigin.java
+++ Battle_1_1/Items/ItemsOrigin.java
@@ -3,17 +3,21 @@ public class ItemsOrigin{
 	private String name;
 	private int[] type;
 	private int[] addAbility;
+	private int attribute;
 	private int money;
 	private Boolean equipment;
+	private int equipmentRestriction;
 	private int[] restriction;
 	private String[] txts;
-	//ItemsOrigin(名前, {アイテムの種類, アイテムの種類の種類}, アイテムの能力, アイテムの価値, 装備できるか, 制限{ダンジョン, 階層}, {アイテムの説明, アイテムの設定});
-	public ItemsOrigin(final String name, final int[] type, final int[] addAbility, final int money, final Boolean equipment, final int[] restriction, final String[] txts){
+	//ItemsOrigin(名前, {アイテムの種類, アイテムの種類の種類}, アイテムの能力, アイテムの属性, アイテムの価値, 装備できるか, 装備できる個数, 制限{ダンジョン, 階層}, {アイテムの説明, アイテムの設定});
+	public ItemsOrigin(final String name, final int[] type, final int[] addAbility, final int attribute, final int money, final Boolean equipment, int equipmentRestriction, final int[] restriction, final String[] txts){
 		this.name = name;
 		this.type = type;
 		this.addAbility = addAbility;
+		this.attribute = attribute;
 		this.money = money;
 		this.equipment = equipment;
+		this.equipmentRestriction = equipmentRestriction;
 		this.restriction = restriction;
 		this.txts = txts;
 	}
@@ -29,11 +33,17 @@ public class ItemsOrigin{
 	public int[] getAddAbility(){
 		return this.addAbility;
 	}
+	public int getAttribute(){
+		return this.attribute;
+	}
 	public int getMoney(){
 		return this.money;
 	}
 	public Boolean getEquipment(){
 		return this.equipment;
+	}
+	public int getEquipmentRestriction(){
+		return this.equipmentRestriction;
 	}
 	public int[] getRestriction(){
 		return this.restriction;
