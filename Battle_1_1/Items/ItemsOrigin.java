70e4c6cb9ea2e56ce01edeef99754db7107c5c70
yukikon1284 <33075631+yukikon1284@users.noreply.github.com>
2017-11-30 14:27:47 +0900

    Battle

--- Battle_1_1/Items/ItemsOrigin.java
+++ Battle_1_1/Items/ItemsOrigin.java
@@ -4,14 +4,18 @@ public class ItemsOrigin{
 	private int[] type;
 	private int[] addAbility;
 	private int money;
-	private String description;
-	//ItemsOrigin(名前, {アイテムの種類, アイテムの種類の種類}, 武器の効果, 武器の説明);
-	public ItemsOrigin(final String name, final int[] type, final int[] addAbility, final int money, final String description){
+	private Boolean equipment;
+	private int[] restriction;
+	private String[] txts;
+	//ItemsOrigin(名前, {アイテムの種類, アイテムの種類の種類}, アイテムの能力, アイテムの価値, 装備できるか, 制限{ダンジョン, 階層}, {アイテムの説明, アイテムの設定});
+	public ItemsOrigin(final String name, final int[] type, final int[] addAbility, final int money, final Boolean equipment, final int[] restriction, final String[] txts){
 		this.name = name;
 		this.type = type;
 		this.addAbility = addAbility;
 		this.money = money;
-		this.description = description;
+		this.equipment = equipment;
+		this.restriction = restriction;
+		this.txts = txts;
 	}
 	public String getName(){
 		return this.name;
@@ -28,7 +32,13 @@ public class ItemsOrigin{
 	public int getMoney(){
 		return this.money;
 	}
+	public Boolean getEquipment(){
+		return this.equipment;
+	}
+	public int[] getRestriction(){
+		return this.restriction;
+	}
 	public String getDescription(){
-		return this.description;
+		return this.txts;
 	}
 }
