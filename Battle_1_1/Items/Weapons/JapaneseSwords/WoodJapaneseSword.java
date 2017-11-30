d4007440731da92b71f26ea332a62e045aea96aa
170135 <34013932+170135@users.noreply.github.com>
2017-11-30 17:24:05 +0900

    Battle

--- Battle_1_1/Items/Weapons/JapaneseSwords/WoodJapaneseSword.java
+++ Battle_1_1/Items/Weapons/JapaneseSwords/WoodJapaneseSword.java
@@ -1,6 +1,6 @@
 package Items.Weapons.JapaneseSwords;
 public class WoodJapaneseSword extends Items.ItemsOrigin{
 	public WoodJapaneseSword(){
-		super("木刀", new int[] {0,3}, new int[] {10,10,10,10,10,10,10,10}, 10, true, new int[] {0,0}, new String[] {"",""});
+		super("木刀", new int[] {0,3}, new int[] {10,10,10,10,10,10,10,10}, 0, 10, true, 1, new int[] {0,0}, new String[] {"",""});
 	}
 }
--- Battle_1_1/Items/Weapons/MagicalSwords/MagicalIronSword.java
+++ Battle_1_1/Items/Weapons/MagicalSwords/MagicalIronSword.java
@@ -1,6 +1,6 @@
 package Items.Weapons.MagicalSwords;
 public class MagicalIronSword extends Items.ItemsOrigin{
 	public MagicalIronSword(){
-		super("魔鉄の剣", new int[] {0,1}, new int[] {10,10,10,10,10,10,10,10}, 10, true, new int[] {0,0}, new String[] {"",""});
+		super("魔鉄の剣", new int[] {0,1}, new int[] {10,10,10,10,10,10,10,10}, 0, 10, true, 1, new int[] {0,0}, new String[] {"",""});
 	}
 }
