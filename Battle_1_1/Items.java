1cde18f26b67dadc143b4ff6ef231fe4b405f014
170135 <34013932+170135@users.noreply.github.com>
2017-11-30 15:36:04 +0900

    Battle

--- Battle_1_1/Items.java
+++ Battle_1_1/Items.java
@@ -5,10 +5,10 @@ public class Items{
 	private static ItemsOrigin[] ItemsOrigin;
 	private static String[][] itemsN = 
 		{{"片手剣","魔法剣","双剣","刀","大剣","槍","杖","魔導銃"}//武器
-		,{"中防具","軽防具","重防具","魔防具"}//防具
+		,{"中量防具","軽量防具","重量防具","魔道防具"}//防具
 		,{"回復薬","特殊魔法薬","その他"}//アイテム{HP、MP回復,能力上昇,その他}
 		,{"攻撃魔法","回復魔法","特殊魔法"}//魔法
-		,{"能力変化","状態異常耐性","その他"}};//装飾品
+		,{"能力上昇","状態異常耐性","その他"}};//装飾品
 	Items(){
 		ItemsOrigin = new ItemsOrigin[] {new WoodSword(), new IronSword(), new BronzeSword(), new MagicalIronSword()};
 		seyDesignationType(0);
