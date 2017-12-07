307451e87f4f447fe23a1100a096b0b9c5979a9f
170135 <34013932+170135@users.noreply.github.com>
2017-12-07 14:03:18 +0900

    Battle

---
+++ Battle_1_1/Items/Weapons/LargeSwords/WoodLargeSword.java
@@ -0,0 +1,6 @@
+package Items.Weapons.LargeSwords;
+public class WoodLargeSword extends Items.ItemsOrigin{
+	public WoodLargeSword(){
+		super("木の大剣", new int[] {0,4}, new int[] {10,10,10,10,10,10,10,10}, new int[][] {{0,0},{0,0}}, 0, 10, true, 1, new int[] {0,0}, new String[] {"",""});
+	}
+}
---
+++ Battle_1_1/Items/Weapons/Spears/WoodSpear.java
@@ -0,0 +1,6 @@
+package Items.Weapons.JapaneseSwords;
+public class WoodSpear extends Items.ItemsOrigin{
+	public WoodSpear(){
+		super("木の槍", new int[] {0,5}, new int[] {10,10,10,10,10,10,10,10}, new int[][] {{0,0},{0,0}}, 10, true, 1, new int[] {0,0}, new String[] {"",""});
+	}
+}
