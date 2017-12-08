0dc7c5b2f49627e9cc0f6ae525c8488fcc48eddd
170135 <34013932+170135@users.noreply.github.com>
2017-12-08 13:55:30 +0900

    battle

--- Battle_1_1/Items/Armors/LightArmors/LeatherLightArmor.java
+++ Battle_1_1/Items/Armors/LightArmors/LeatherLightArmor.java
@@ -1,6 +1,6 @@
 package Items.Armors.LightArmors;
 public class LeatherLightArmor extends Items.ItemsOrigin{
 	public LeatherLightArmor(){
-		super("革の軽防具", new int[] {1,1}, new int[] {10,10,10,10,10,10,10,10}, new int[][] {{0,0},{0,0}}, 10, true, 1, new int[] {0,0}, new String[] {"",""});
+		super("革の軽防具", new int[] {1,1}, new int[] {10,10,10,10,10,10,10,10}, new int[][] {{0,0,0,0,0},{0,0,0,0,0}}, 10, true, 1, new int[] {0,0}, new String[] {"",""});
 	}
 }
--- Battle_1_1/Items/Weapons/OneHandedSwords/Hinoki.java
+++ Battle_1_1/Items/Weapons/OneHandedSwords/Hinoki.java
@@ -1,6 +1,6 @@
 package Items.Weapons.OneHandedSwords;
 public class Hinoki extends Items.ItemsOrigin{
 	public Hinoki(){
-		super("ひのきの棒", new int[] {0,0}, new int[] {10,10,10,10,10,10,10,10}, new int[][] {{0,0},{0,0}}, 10, true, 1, new int[] {0,0}, new String[] {"",""});
+		super("ひのきの棒", new int[] {0,0}, new int[] {10,10,10,10,10,10,10,10}, new int[][] {{0,0,0,0,0},{0,0,0,0,0}}, 10, true, 1, new int[] {0,0}, new String[] {"",""});
 	}
 }
